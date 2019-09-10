bb22048e695425509242ca8c367c450fcdd14207
Yevhenii <yevgenii.andrieiev@innovecs.com>
2019-09-10 11:14:57 +0300

    added service

--- angular-crash-course/angular-app/src/app/app.module.ts
+++ angular-crash-course/angular-app/src/app/app.module.ts
@@ -1,18 +1,23 @@
+import { CoursesService } from './courses.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    CoursesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- angular-crash-course/angular-app/src/app/courses.component.ts
+++ angular-crash-course/angular-app/src/app/courses.component.ts
@@ -1,9 +1,24 @@
+import { CoursesService } from './courses.service';
 import { Component } from '@angular/core';
 
 @Component({
     selector: 'cc-courses',
-    template: '<h2>Courses</h2>'
+    template: `
+        <h2>{{ title }}</h2>
+        <ul>
+            <li *ngFor="let course of courses">
+                {{ course }}
+            </li>
+        </ul>
+        <img [src]="imageUrl" />
+    `
 })
 export class CoursesComponent {
+    title = 'List of Courses';
+    courses;
+    imageUrl = 'http://lorempixel.com/400/200';
 
+    constructor(service: CoursesService) {
+        this.courses = service.getCourses();
+    }
 }
