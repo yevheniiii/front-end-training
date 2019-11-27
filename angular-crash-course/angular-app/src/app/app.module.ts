98cf269bb44ced1e43ab5c7c101fe3fd4ccb30a0
Yevgenii Andrieiev <Yevgenii.Andrieiev@innovecs.com>
2019-11-27 22:19:00 +0200

    finished section with reactive forms

--- angular-crash-course/angular-app/src/app/app.module.ts
+++ angular-crash-course/angular-app/src/app/app.module.ts
@@ -1,23 +1,17 @@
-import { CoursesService } from './courses.service';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { CoursesService } from "./courses.service";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CoursesComponent } from './courses.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { CoursesComponent } from "./courses.component";
+import { SignupFormComponent } from "./signup-form/signup-form/signup-form.component";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CoursesComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    CoursesService
-  ],
+  declarations: [AppComponent, CoursesComponent, SignupFormComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, FormsModule],
+  providers: [CoursesService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
